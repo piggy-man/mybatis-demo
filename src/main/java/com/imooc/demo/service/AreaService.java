3f7ebd9db639dde1a3b30d5241cf99869d339bc6
Gavin <1146855407@qq.com>
2018-04-26 14:23:20 +0800

    Initial commit

---
+++ src/main/java/com/imooc/demo/service/AreaService.java
@@ -0,0 +1,12 @@
+package com.imooc.demo.service;
+import com.imooc.demo.entity.Area;
+import java.util.List;
+
+public interface AreaService {
+    List<Area> getAreaList();
+    Area queryAreaById(int areaId);
+    boolean addArea(Area area);
+    boolean modifyArea(Area area);
+    boolean deleteArea(int areaId);
+
+}
